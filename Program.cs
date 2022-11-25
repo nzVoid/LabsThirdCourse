5c5eef04d92c0ba4897965487e431b5bcfe5a377
nzVoid <yakunin901@gmail.com>
2022-11-25 15:12:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using static lab2;
+
+class LabsThirdCourse
+{
+    public static void function()
+    {
+        Console.WriteLine("1 = exit, 2 = restart");
+        int mod = Convert.ToInt16(Console.ReadLine());
+        switch (mod)
+        {
+            case 1:
+                Console.WriteLine("Bye!");
+                break;
+            case 2:
+                Console.Clear();
+                Main();
+                break;
+        }
+    }
+
+    public static void choose_var(int var)
+    {
+        switch (var)
+        {
+            case 1:
+                Console.WriteLine("not found");
+                function();
+                break;
+            case 2:
+                lab2.var2();
+                function();
+                break;
+            case 3:
+                lab2.var3();
+                function();
+                break;
+        }
+
+    }
+
+    static int Main()
+    {
+        Console.WriteLine("All labs of C#");
+        Console.WriteLine("Choose lab: (start with 2)");
+        int choose_lab = Convert.ToInt16(Console.ReadLine());
+        switch (choose_lab)
+        {           
+            case 2:
+                Console.WriteLine("Lab 2");
+                Console.Write("Choose var: ");
+                int variant = Convert.ToInt16(Console.ReadLine());
+                choose_var(variant);
+                break;
+            default:
+                Console.WriteLine("Choose corectly number!");
+                Main();
+                break;
+        }
+        
+        return 0;
+    }
+}
---
+++ lab2.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class lab2
+{
+    public static bool triangle(float x, float y,
+            float x1, float y1,
+            float x2, float y2,
+            float x3, float y3)
+    {
+        float a = (x - x1) * (y - y2) - (x - x2) * (y - y1),
+            b = (x - x2) * (y - y3) - (x - x3) * (y - y2),
+            c = (x - x3) * (y - y1) - (x - x1) * (y - y3);
+        return ((a >= 0 && b >= 0 && c >= 0) ||
+            (a <= 0 && b <= 0 && c <= 0));
+    }
+
+    public static void var2()
+    {   
+        Console.WriteLine("Choose task: ");
+        int task = Convert.ToInt32(Console.ReadLine());
+        switch (task) {
+            case 1:
+                Console.WriteLine("Task 1");
+                int x, y, x1 = -1, y1 = -1, x2 = 1, y2 = -1, x3 = 0, y3 = 0;
+                x = Convert.ToInt16(Console.ReadLine());
+                y = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine(triangle(x, y, x1, y1, x2, y2, x3, y3));
+                break;
+            case 2:
+                Console.WriteLine("Task 2");
+                Console.WriteLine("x = -2");
+                double xx = -2;
+                double yy;
+                if(xx < 0)
+                {
+                    yy = 2 * Math.Cos(xx - 1);
+                    Console.WriteLine("y = " + yy);
+                }
+                else if(0<=xx && xx <= 3 * 3.14)
+                {
+                    yy = 1;
+                    Console.WriteLine("y = " + yy);
+                }
+                else if (xx > 3 * 3.14)
+                {
+                    yy = Math.Sin(xx + 1);
+                    Console.WriteLine("y = " + yy);
+                }
+
+                Console.WriteLine("x = 1");
+                xx = 1;
+                if (xx < 0)
+                {
+                    yy = 2 * Math.Cos(xx - 1);
+                    Console.WriteLine("y = " + yy);
+                }
+                else if (0 <= xx && xx <= 3 * 3.14)
+                {
+                    yy = 1;
+                    Console.WriteLine("y = " + yy);
+                }
+                else if (xx > 3 * 3.14)
+                {
+                    yy = Math.Sin(xx + 1);
+                    Console.WriteLine("y = " + yy);
+                }
+
+                Console.WriteLine("x = 8");
+                xx = 8;
+                if (xx < 0)
+                {
+                    yy = 2 * Math.Cos(xx - 1);
+                    Console.WriteLine("y = " + yy);
+                }
+                else if (0 <= xx && xx <= 3 * 3.14)
+                {
+                    yy = 1;
+                    Console.WriteLine("y = " + yy);
+                }
+                else if (xx > 3 * 3.14)
+                {
+                    yy = Math.Sin(xx + 1);
+                    Console.WriteLine("y = " + yy);
+                }
+                break;
+            case 3:
+                var days = new Dictionary<int, int>()
+                {
+                    [1]=3,
+                    [2]=1,
+                    [3]=4,
+                    [4]=2,
+                    [5]=2,
+                };
+                Console.WriteLine("Task 3");
+                Console.WriteLine("Choose day: ");
+                int day = Convert.ToInt16(Console.ReadLine());
+                switch (day)
+                {
+                    case 1:
+                        Console.WriteLine("Day " + day + " value of lesson " + days[day]);
+                        break;
+                    case 2:
+                        Console.WriteLine("Day " + day + " value of lesson " + days[day]);
+                        break;
+                    case 3:
+                        Console.WriteLine("Day " + day + " value of lesson " + days[day]);
+                        break;
+                    case 4:
+                        Console.WriteLine("Day " + day + " value of lesson " + days[day]);
+                        break;
+                    case 5:
+                        Console.WriteLine("Day " + day + " value of lesson " + days[day]);
+                        break;
+                }
+                break;
+        }
+    }
+
+    public static void var3()
+    {
+        Console.WriteLine("Choose task: ");
+        int task = Convert.ToInt32(Console.ReadLine());
+        switch (task)
+        {
+            case 1:
+                Console.WriteLine("Task 1");
+                
+                break;
+            case 2:
+                Console.WriteLine("Task 2");
+
+                break;
+            case 3:
+                Console.WriteLine("Task 3");
+               
+                break;
+        }
+    }
+}
+
